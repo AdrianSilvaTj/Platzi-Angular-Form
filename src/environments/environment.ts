f4261f8d3e0635156caafa3cc5e1f4eaa3db7743
AdrianSilvaTj <adrian.silva.tj@gmail.com>
2023-06-06 10:55:34 -0500

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url_api: 'https://store.nicobytes.site/api',
+  firebase: {
+    projectId: 'platzi-angular-form',
+    appId: '1:167718191313:web:15e933404b32a59c6d45f0',
+    storageBucket: 'platzi-angular-form.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCmG5bUM97GuMVTsH_S8EX8LIipRQBWZvs',
+    authDomain: 'platzi-angular-form.firebaseapp.com',
+    messagingSenderId: '167718191313',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
