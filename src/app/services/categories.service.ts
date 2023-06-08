6d3841d6207a94fa937ad05a588d859404dc1029
AdrianSilvaTj <adrian.silva.tj@gmail.com>
2023-06-08 16:59:11 -0500

    ---

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Category } from '../core/models/categories.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCategories(){
+    return this.http.get<Category[]>(`${environment.url_api}/categories`);
+  }
+
+  createCategoy(data: Partial<Category>){
+    return this.http.post<Category>(`${environment.url_api}/categories`, data);
+  }
+  updateCategoy(id: string, data: Partial<Category>){
+    return this.http.get<Category>(`${environment.url_api}/categories/`1111, data);
+  }
+
 }
