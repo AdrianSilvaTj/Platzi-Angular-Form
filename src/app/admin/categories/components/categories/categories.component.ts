6eace0fde1f42b9c949d82c61d5c5b515efc9566
AdrianSilvaTj <adrian.silva.tj@gmail.com>
2023-06-09 12:00:01 -0500

    ---

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/core/models/categories.model';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.showAll();
+  }
+  showAll() {
+    this.categoriesService.getAllCategories().subscribe(categories => {
+      this.categories = categories;
+    })
   }
 
 }
